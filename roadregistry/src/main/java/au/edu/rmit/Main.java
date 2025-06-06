9db4a7790bc963b8c73d86146545cf95574d770f
Rebecca Phan <s4093651@student.rmit.edu.au>
2025-06-06 22:09:03 +1000

    workflow comment

--- roadregistry/src/main/java/au/edu/rmit/Main.java
+++ roadregistry/src/main/java/au/edu/rmit/Main.java
@@ -50,7 +50,8 @@ public static void main(String[] args) {
     LocalDate goodOffenseDate = LocalDate.parse("02-06-2023", DateTimeFormatter.ofPattern("dd-MM-yyyy"));
     String demeritSuccess = personSuccessAdd.addDemeritPoints(goodOffenseDate, 5); // ✅ Valid
     System.out.println("Demerit update result: " + demeritSuccess);
-    System.out.println("workflow test success");
+    
 }
-}
+} // workflow showcase
+
 // Trigger workflow
